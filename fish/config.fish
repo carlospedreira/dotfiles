b9e1af2b2dde9cc7ef7c6b17836c2c1c91e4406c
Carlos Pedreira <carlospedreira@users.noreply.github.com>
2026-04-22 01:43:33 +0100

    fish: add sesh aliases, remove clone/pull aliases

--- fish/config.fish
+++ fish/config.fish
@@ -1,11 +1,11 @@
 if status is-interactive
     starship init fish | source
 
-    alias clone="~/.local/bin/clone-azure-repos.sh"
-    alias pull="~/.local/bin/git-pull-all.sh"
     alias ls="eza -lh --icons --group-directories-first"
     alias ll="eza -lh --icons --group-directories-first"
     alias la="eza -lah --icons --group-directories-first"
     alias lt="eza --tree --level=2 --icons"
     alias cat="bat"
+    alias s="sesh picker"
+    alias sc="sesh connect"
 end
