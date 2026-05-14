e5427832dbff007a51af8f69d5d3fffabd0889a3
Carlos Pedreira <carlospedreira@users.noreply.github.com>
2026-05-14 01:12:33 +0100

    Update .config/shell/env.sh

--- dot_config/shell/env.sh
+++ dot_config/shell/env.sh
@@ -1,6 +1,5 @@
 export HOMEBREW_BUNDLE_FILE="$XDG_CONFIG_HOME/homebrew/Brewfile"
 export HOMEBREW_NO_ENV_HINTS=1
 export DOTNET_CLI_TELEMETRY_OPTOUT=1
-export DO_NOT_TRACK=1
 export EDITOR="nvim"
 export PAGER="less"
