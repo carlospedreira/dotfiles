e695de030ab1202dae796a4b67fff132adf4fe5d
Carlos Pedreira <carlospedreira@users.noreply.github.com>
2026-06-08 01:36:19 +0100

    Simplify script by removing 'apt' check
    Removed check for 'apt' command and added autoremove.

--- .chezmoiscripts/run_before_ubuntu_install-prerequisites.sh
+++ .chezmoiscripts/run_before_ubuntu_install-prerequisites.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-if ! command -v apt >/dev/null 2>&1; then
-    exit 0
-fi
-
 sudo apt update
 sudo apt install -y curl git ca-certificates libatomic1 libicu78 build-essential bubblewrap zsh
+sudo apt autoremove -y
