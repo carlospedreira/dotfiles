e71ce7aedbda862129cfa480ba1b55d7585ec3c5
Carlos Pedreira <carlospedreira@users.noreply.github.com>
2026-07-14 16:59:35 +0100

    Sort shell environment variables

--- dot_config/shell/env.sh
+++ dot_config/shell/env.sh
@@ -1,6 +1,6 @@
-export HOMEBREW_BUNDLE_FILE="$XDG_CONFIG_HOME/homebrew/Brewfile"
-export HOMEBREW_NO_ENV_HINTS=1
 export DOTNET_CLI_TELEMETRY_OPTOUT=1
 export EDITOR="nvim"
+export HOMEBREW_BUNDLE_FILE="$XDG_CONFIG_HOME/homebrew/Brewfile"
+export HOMEBREW_NO_ENV_HINTS=1
 export PAGER="less"
 export SHELL="/bin/zsh"
